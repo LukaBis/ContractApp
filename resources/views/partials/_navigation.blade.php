f2634b6d980465ee49a2c0e0767e9d3eb7f85f5b
Luka <LukaBis@lbiskupic05@gmail.com>
2023-02-26 19:16:53 +0100

    quick

--- resources/views/partials/_navigation.blade.php
+++ resources/views/partials/_navigation.blade.php
@@ -1,6 +1,6 @@
 <nav class="navbar is-info" role="navigation" aria-label="main navigation">
   <div class="navbar-brand">
-    <a class="navbar-item" href="https://bulma.io">
+    <a class="navbar-item" href="/">
         <b>CRYPTO</b>
     </a>
 
