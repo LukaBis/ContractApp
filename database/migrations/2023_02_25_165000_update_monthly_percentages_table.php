4da0e0de67b20f42cefa301f8f977555dd1aefd7
Luka <LukaBis@lbiskupic05@gmail.com>
2023-02-26 19:31:10 +0100

    remove nullable

--- database/migrations/2023_02_25_165000_update_monthly_percentages_table.php
+++ database/migrations/2023_02_25_165000_update_monthly_percentages_table.php
@@ -12,7 +12,7 @@ return new class extends Migration
     public function up(): void
     {
         Schema::table('monthly_percentages', function (Blueprint $table) {
-            $table->float('usd_ratio')->nullable();
+            $table->float('usd_ratio');
         });
     }
 
